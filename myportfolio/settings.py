778fe4e51906ed73abcabf41a8c83ca046def167
sorelleTchie <sorelletchie@gmail.com>
2023-07-28 14:24:39 +0100

    modifications made to settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -138,11 +138,12 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'sorelletchie@gmail.com'
-EMAIL_HOST_PASSWORD = 'your-email-password'  # Change to your Gmail password
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 import django_heroku
 django_heroku.settings(locals())
