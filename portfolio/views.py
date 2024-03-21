05a19560193e605564cb60bd432122a271c307f8
SorelleTchie <sorelletchie@gmail.com>
2024-03-21 11:11:20 +0100

    modified robot.txt, and added keyword

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import ContactForm
 from django.core.mail import send_mail, BadHeaderError
+from django.views import View
 
 
 # Create your views here.
@@ -20,25 +21,7 @@ def cv(request):
 def contact(request):
     return render(request, "contact.html")
     
+ 
     
     
-    """if request.method == 'POST':
-        form = ContactForm(request.POST)
-        if form.is_valid():
-            name = form.cleaned_data['name']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-
-            send_mail(
-                'Contact Form Message',
-                f'Message from {name} <{email}>:\n\n{message}',
-                email,
-                  ['sorelletchie@gmail.com'],  # Change to your email address
-            )
-            
-            return redirect('success')
-    else:
-        form = ContactForm()
-    
-    return render(request, 'contact.html', {'form': form}) """
   
