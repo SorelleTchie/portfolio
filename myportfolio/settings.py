da5a140429e843c5fd273c82e55cf752df773e97
SorelleTchie <sorelletchie@gmail.com>
2024-10-29 09:21:36 +0100

    added email host to env file

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -143,7 +143,7 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'email-smtp.eu-north-1.amazonaws.com'
+EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
