69c678a65fda4e38822ce091f5c9e756fff5ae43
SorelleTchie <sorelletchie@gmail.com>
2025-01-13 10:33:59 +0100

    added allowed host in settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://main.d3osmlb2ujvpp9.amplifyapp.com/']
 
 
 # Application definition
