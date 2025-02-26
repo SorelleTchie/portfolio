3d75fd23729c26719437691cf0f57f62954a1c15
SorelleTchie <sorelletchie@gmail.com>
2025-02-26 09:07:49 +0100

    modified settings.py allowed_Hosts

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://main.d3osmlb2ujvpp9.amplifyapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
